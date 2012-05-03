b34b05d31e333d504f6c9dd077a4ca148bb37d9e
Mike Pall <mike>
2012-05-03 21:16:28 +0200

    Add description to example code.

--- testgpio.lua
+++ testgpio.lua
@@ -1,3 +1,7 @@
+-- Simple example for Raspberry Pi GPIO module.
+-- Blinks GPIO 0 (LED + resistor to 0V) and shows input status of GPIO 6.
+-- Written by Mike Pall. Public domain.
+
 local gpio = require("rpi.gpio")
 
 for i=1,10 do
